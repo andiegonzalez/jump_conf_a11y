9b19c34fc664adfef2cebe957d266f3747ff00c6
Andie Gonzalez <a.gzzart@gmail.com>
2021-08-17 22:52:20 -0500

    resetear al punto de inicio

--- src/App.js
+++ src/App.js
@@ -1,37 +1,6 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.css";
-import Form from "./components/Form";
-import jump_logo from "./assets/jump_logo.webp";
 
-const App = () => {
-  const [state, setState] = useState({ formSubmitted: false, formData: {} });
-  const onSubmit = (event, data) => {
-    event.preventDefault();
-    if (data.name) {
-      setState((prevState) => ({
-        ...prevState,
-        formSubmitted: true,
-        formData: { ...data },
-      }));
-    }
-  };
-
-  return (
-    <main>
-      <img src={jump_logo} alt="Jump Conference Logo" />
-      <header>
-        <h1>Aprendiendo de Accesibilidad con React</h1>
-        <p className="tagline">JumpConf 2021</p>
-      </header>
-      {state.formSubmitted && (
-        <div aria-live="assertive" className="form-feedback">
-          <p>Registro Exitoso!</p>
-          <p>Gracias {state.formData.name}, te vemos pronto!</p>
-        </div>
-      )}
-      <Form onSubmit={onSubmit} />
-    </main>
-  );
-};
+const App = () => <div>{/* Aqui crearemos nuestro proyecto */}</div>;
 
 export default App;
--- src/components/Input.js
+++ src/components/Input.js
@@ -1,31 +1,5 @@
 import React from "react";
 
-import calendarIcon from "../assets/calendar-page-empty.svg";
-import envelopeIcon from "../assets/envelope.svg";
-import userIcon from "../assets/user-shape.svg";
-
-const icons = {
-  age: calendarIcon,
-  email: envelopeIcon,
-  name: userIcon,
-};
-
-const Input = (props) => {
-  const { label, type, name, onChange } = props;
-  return (
-    <div>
-      <img
-        aria-hidden="true"
-        width="20px"
-        height="20px"
-        src={icons[name]}
-        alt={`${name} icon`}
-        style={{ margin: "0 10px 0 0" }}
-      />
-      <label htmlFor={name}>{label}</label>
-      <input type={type} name={name} id={name} onChange={onChange} />
-    </div>
-  );
-};
+const Input = () => <>{/* Aqui crearemos nuestro componente de <Form/> */}</>;
 
 export default Input;
